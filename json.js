a3f445cde40637f6aeb744b1e53643ac8a519891
ogbeche77 <ogbeche77@yahoo.com>
2020-02-12 09:53:49 +0100

    comments added

--- json.js
+++ json.js
@@ -1,6 +1,7 @@
 //handling json data using express & body parser
 //http post request and body parser, used for form login
 //npm body parser used for this project
+//jquery data is posted using ajax
 
 const express = require("express"); // when we require express module, we get back function
 const path = require("path");
@@ -9,15 +10,16 @@ const exp = express();
 
 exp.use("/public",express.static(path.join(__dirname,"static")));
 exp.use(bodyParser.urlencoded({extended:false}));
-exp.use(bodyParser.json());
+exp.use(bodyParser.json()); //parse json & attach to request.body
 
 exp.get("/",(req,res)=>{
 res.sendFile (path.join(__dirname, "static", "form1.html"));
 });
 
-exp.post("/",(req,res)=>{
+//database section here
+    exp.post("/",(req,res)=>{
     console.log(req.body);
-    //database section here
+    
     res.json({sucess :true});
 });
 
