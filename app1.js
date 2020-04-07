7e3ad55ed441bd14cdc2c9cd1aac0666197f1d36
ogbeche77 <ogbeche77@yahoo.com>
2020-04-07 11:52:07 +0200

    updated

--- app1.js
+++ app1.js
@@ -3,9 +3,9 @@
 const sum = (num1, num2) => num1 + num2;
 const pi = 3.14;
 class SomeMath {
-    constructor(){
-        console.log("object");
-    }
+  constructor() {
+    console.log("object");
+  }
 }
 module.exports.sum = sum;
 module.exports.pi = pi;
