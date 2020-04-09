fda99a0d32d27d7147370e2391ca281b41955d0d
ogbeche77 <ogbeche77@yahoo.com>
2020-04-09 12:57:32 +0200

    exp

--- express1.js
+++ express1.js
@@ -1,17 +1,17 @@
 const express = require("express"); // when we require express module, we get back function
 const exp = express();
-exp.get("/",(req,res)=>{
-res.send ("Hi my friend")
+exp.get("/", (req, res) => {
+  res.send("Hi my friend");
 });
 
-exp.get("/home",(req,res)=>{
-    res.send ("Hi my friend, are you home?")
-    });
+exp.get("/home", (req, res) => {
+  res.send("Hi my friend, are you home?");
+});
 
-    exp.get("/home/:name/:age", (req,res)=>{
-        console.log(req.params);
-        console.log(req.query);// useful for optional setting 
-        res.send(req.params.name + " : " + req.params.age );
-    });
-    
+exp.get("/home/:name/:age", (req, res) => {
+  console.log(req.params);
+  console.log(req.query); // useful for optional setting
+  res.send(req.params.name + " : " + req.params.age);
+});
+
 exp.listen(3000);
