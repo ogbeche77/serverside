37d3db8648949f2e146b9d5a07e85c4e62c8627c
ogbeche77 <ogbeche77@yahoo.com>
2020-04-09 12:58:34 +0200

    write file

--- folder.js
+++ folder.js
@@ -1,23 +1,18 @@
 //make folder and file inside the folder folder
 const fs = require("fs");
-fs.mkdir("tutorial", (err)=>{ //mkdir means make directory
-  if(err)
-  console.log(err);
-  else{
-      fs.writeFile("./tutorial/example.txt", "I am here", (err)=>{ //file path,content,3rd argiment
-         if(err)
-         console.log(err);
-         else {
-             console.log("file created");
-         }
-      });
-
+fs.mkdir("tutorial", (err) => {
+  //mkdir means make directory
+  if (err) console.log(err);
+  else {
+    fs.writeFile("./tutorial/example.txt", "I am here", (err) => {
+      //file path,content,3rd argiment
+      if (err) console.log(err);
+      else {
+        console.log("file created");
+      }
+    });
   }
-  });
-
-
-     
-
+});
 
 //deleting a folder that also has a file
 
