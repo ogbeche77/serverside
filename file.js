39b655c4635df61550acaae18789594846153b2c
ogbeche77 <ogbeche77@yahoo.com>
2020-04-09 12:58:23 +0200

    read file

--- file.js
+++ file.js
@@ -2,18 +2,14 @@
 
 const fs = require("fs");
 //create file, 1st argument is name of file, 2nd is content of file,3rd is call back
-fs.writeFile("example.txt", "Hier ist ein beispiel", (err)=>{
-    if(err)
-    console.log(err);
-    else console.log("File created");
-    fs.readFile("example.txt",  "utf8", (err, file)=>{ //utf8 encoding ensures content is read
-        if(err)
-        console.log (err);
-        else console.log(file);
-
-    });
-
-
+fs.writeFile("example.txt", "Hier ist ein beispiel", (err) => {
+  if (err) console.log(err);
+  else console.log("File created");
+  fs.readFile("example.txt", "utf8", (err, file) => {
+    //utf8 encoding ensures content is read
+    if (err) console.log(err);
+    else console.log(file);
+  });
 });
 /*//renaming file
 const fs = require("fs");
@@ -26,8 +22,6 @@ console.log("file renamed");
 });
 
 */
-
-
 
 //adding content to file
 
