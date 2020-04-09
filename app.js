bdc64c51c6f4c56d08a89a28a0c8dd29a49e8886
ogbeche77 <ogbeche77@yahoo.com>
2020-04-09 12:55:57 +0200

    update

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 //working with modules
 const app1 = require("./app1"); //means app1 is in same path with app
-console.log (app1.sum(1,1));
-console.log (app1.pi);
-console.log (new app1.SomeMath());
-
-
-
-
+console.log(app1.sum(1, 1));
+console.log(app1.pi);
+console.log(new app1.SomeMath());
