709ad3df76060b09d4acd736f232a7677fe1ee89
ogbeche77 <ogbeche77@yahoo.com>
2020-04-17 10:16:01 +0200

    updated

--- string.js
+++ string.js
@@ -3,33 +3,31 @@
 const Joi = require("joi");
 
 const arrayString = ["banana", "bacon", "cheese"];
-const arrayObjects = [{example: "example1"},{example: "example2"}];
+const arrayObjects = [{ example: "example1" }, { example: "example2" }];
 
-const userInput = { personalInfo: {
-                        streetAddress : "12398787",
-                        city : "lagos",
-                        state: "ibadan"
-                    },
-                    preferences: arrayString
-                };
+const userInput = {
+  personalInfo: {
+    streetAddress: "12398787",
+    city: "lagos",
+    state: "ibadan",
+  },
+  preferences: arrayString,
+};
 
 const personalInfoSchema = Joi.object().keys({
-    streetAddress : Joi.string().trim().required(),
-    city : Joi.string().trim().required(),
-    state: Joi.string().trim().length(2).required(),
-
+  streetAddress: Joi.string().trim().required(),
+  city: Joi.string().trim().required(),
+  state: Joi.string().trim().length(2).required(),
 });
 
 const preferencesSchema = Joi.array().items(Joi.string());
 
 const schema = Joi.object().keys({
-    personalInfo : personalInfoSchema,
-    preferences : preferencesSchema
+  personalInfo: personalInfoSchema,
+  preferences: preferencesSchema,
 });
 
-Joi.validate(userInput, schema,(err,result)=>{
-if(err)
-console.log (err);
-else
-    console.log(result);
+Joi.validate(userInput, schema, (err, result) => {
+  if (err) console.log(err);
+  else console.log(result);
 });
