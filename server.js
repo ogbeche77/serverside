e7dd501a28ab26f08ede1240a1c5e5ff44140098
ogbeche77 <ogbeche77@yahoo.com>
2020-04-21 09:56:49 +0200

    server

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const http = require("http");
-const server = http.createServer((req,res)=>{ //req is what client request
-    if(req.url === "/"){
-        res.write("Hello mehn");
-        res.end(); //sends request to client
-    }
+const server = http.createServer((req, res) => {
+  //req is what client request
+  if (req.url === "/") {
+    res.write("Hello mehn");
+    res.end(); //sends request to client
+  }
 });
 
 server.listen("3000");
